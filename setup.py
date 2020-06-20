4ad9bbbc307e9fd40cd052e73f532180571d5905
Kris Warner <kdwarn@protonmail.com>
2020-06-19 23:06:10 -0400

    Update to latest Click version

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 from setuptools import setup
 from setuptools import find_packages
 
-DESCRIPTION = ("CLI application that focus on quickly creating "
-               "study flash cards")
+DESCRIPTION = "CLI application that focus on quickly creating study flash cards"
 
-REQUIREMENTS = ['click==6.6']
+REQUIREMENTS = ["click==7.1"]
 
 setup(
-    name='pyflashcards',
-    author='Jonathan Lalande',
-    author_email='jonathan.lalande.1@ens.etsmtl.ca',
-    version='1.0.2',
-    license='MIT',
+    name="pyflashcards",
+    author="Jonathan Lalande",
+    author_email="jonathan.lalande.1@ens.etsmtl.ca",
+    version="1.0.2",
+    license="MIT",
     description=DESCRIPTION,
     packages=find_packages(),
     install_requires=REQUIREMENTS,
-    data_files=[('/etc/bash_completion.d/', ['flashcards-complete.sh'])],
+    data_files=[("/etc/bash_completion.d/", ["flashcards-complete.sh"])],
     entry_points="""
         [console_scripts]
         flashcards=flashcards.main:cli
-    """
+    """,
 )
